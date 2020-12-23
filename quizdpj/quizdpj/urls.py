f124e958da05df924d99f072da492fd0fd351a5d
Almir Vinicius <a.vinicius.12@gmail.com>
2020-12-23 17:06:59 -0300

    alteracoes
    alterando

--- quizdpj/quizdpj/urls.py
+++ quizdpj/quizdpj/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('polls/<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
