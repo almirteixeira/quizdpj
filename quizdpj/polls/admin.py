1bca76cb1fa2f46fd3e4b27def5fb49134fff6c5
Almir Vinicius <a.vinicius.12@gmail.com>
2020-12-24 20:04:08 -0300

    alteracao 24 de dezembro

--- quizdpj/polls/admin.py
+++ quizdpj/polls/admin.py
@@ -1,6 +1,53 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import *
+from django.contrib.auth.models import User
+from django.utils.html import format_html
+from django.urls import *
 
-from .models import Question
+from .models import Question, Client, Product
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('cnpj', 'name', 'code')
+    fields = ('user',  'cnpj', 'name', 'code', 'year', 'active')
+    readonly_fields = ['code']
+    search_fields = ['name']
+
+    def get_queryset(self, request):
+        query = super(ClientAdmin, self).get_queryset(request)
+        filtered_query = query.filter(active=True, year=2019)
+        return filtered_query
+
+    
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('client_form', 'participation', 'segment', 'condition', 'layout',
+                    'layoutuqbar', 'status')
+
+    readonly_fields = ['layout', 'status']
+    search_fields = ['client__name',
+                     'client__cnpj', 'participation', 'segment']
+    list_filter = ['participation', 'segment',
+                   'condition', 'layout', 'layoutuqbar', 'status']
+
+    def get_queryset(self, request):
+        query = super(ProductAdmin, self).get_queryset(request)
+        filtered_query = query.filter(client__active=True)
+        return filtered_query
+
+    
+    def client_form(self, obj):
+        return format_html(
+            str(obj.client)
+        )
+
+
+
+
+
+
 
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product, ProductAdmin)
--- quizdpj/polls/models.py
+++ quizdpj/polls/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.db.models import Model
+from django.db.models import CharField, BooleanField, NullBooleanField, FileField, IntegerField, EmailField, TextField, ImageField
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+from django.conf import settings
+from django.contrib.auth.hashers import make_password
+
 
 # Create your models here.
 class Question(models.Model):
@@ -16,3 +24,73 @@ class Choice(models.Model):
         return self.choice_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+
+class Client(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, null=True)
+    cnpj = CharField(max_length=255, verbose_name=u'CNPJ')
+    name = CharField(max_length=200, verbose_name=u'Apelido da Instituição')
+    code = CharField(max_length=200, verbose_name=u'Código')
+    year = IntegerField()
+    active = NullBooleanField(verbose_name=u'Ativo')
+
+    def __str__(self):
+        return self.name + ' - ' + self.cnpj
+
+
+class Product(models.Model):
+    PARTICIPATION_CHOICES = [('Abertura de Segmento', 'Abertura de Segmento'),
+                             ('Página Exclusiva', 'Página Exclusiva'),
+                             ('Contra Capa', 'Contra Capa'),
+                             ('Ipanema', 'Ipanema')]
+
+    SEGMENT_CHOICES = [('Administradores', 'Administradores'),
+                       ('Advogados', 'Advogados'),
+                       ('Agentes Fiduciários', 'Agentes Fiduciários'),
+                       ('Auditores', 'Auditores'),
+                       ('Avaliadores e Classificadoras',
+                        'Avaliadores e Classificadoras'),
+                       ('Consultores', 'Consultores'),
+                       ('Custodiantes', 'Custodiantes'),
+                       ('Distribuidores', 'Distribuidores'),
+                       ('Estruturadores', 'Estruturadores'),
+                       ('Fintechs', 'Fintechs'),
+                       ('Gestores', 'Gestores'),
+                       ('Informação e Análise', 'Informação e Análise'),
+                       ('Investidores Institucionais',
+                        'Investidores Institucionais'),
+                       ('Securitizadoras', 'Securitizadoras'),
+                       ('Servicers e Agentes de Cobrança',
+                        'Servicers e Agentes de Cobrança'),
+                       ('Tecnologia da Informação', 'Tecnologia da Informação'),
+                       ('Lawtechs', 'Lawtechs'),
+                       ('Seguradoras', 'Seguradoras'),
+                       ('Infraestrutura de Mercado', 'Infraestrutura de Mercado'),
+                       ('', '')]
+
+    CONDITION_CHOICES = [('Pagante', 'Pagante'), ('Cortesia', 'Cortesia')]
+
+    STATUS_CHOICES = [('Não Iniciado', 'Não Iniciado'), ('Incompleto',
+                                                         'Incompleto'), ('Concluído', 'Concluído'), ('Aprovado', 'Aprovado')]
+
+    client = models.ForeignKey(
+        Client, verbose_name=u'Cliente', on_delete=models.CASCADE)
+    participation = CharField(
+        max_length=100, verbose_name=u'Participação', choices=PARTICIPATION_CHOICES)
+    segment = CharField(max_length=100, verbose_name=u'Segmento',
+                        choices=SEGMENT_CHOICES, blank=True)
+    condition = CharField(
+        max_length=50, verbose_name=u'Condição', choices=CONDITION_CHOICES)
+    layout = NullBooleanField(verbose_name=u'Layout')
+    layoutuqbar = NullBooleanField(verbose_name=u'Layout UQBAR')
+    status = CharField(max_length=50, verbose_name=u'Status',
+                       choices=STATUS_CHOICES, default='Não Iniciado')
+
+    def __str__(self):
+        if self.segment == '':
+            return 'Produto ' + str(self.id)
+        else:
+            return 'Produto ' + str(self.id)
